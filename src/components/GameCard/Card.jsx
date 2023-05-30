f7f278d54e8804dc51c996ee2383cf0f1c4bd527
Diego_Parra <103591603+diego072117@users.noreply.github.com>
2023-05-29 20:53:51 -0500

    feat(paginate): paginate games.

--- src/components/GameCard/Card.jsx
+++ src/components/GameCard/Card.jsx
@@ -7,8 +7,13 @@ export const Card = ({ data }) => {
         {data.map((game) => (
           <div className="card" key={game.id}>
             <img src={game.thumbnail} className="img-card" alt="Imagen" />
-            <div>
-              <p>{game.title}</p>
+            <div className="content-container">
+              <div className="title-game">
+                {game.title}
+              </div>
+              <div className="card-icon">
+                <i className="fa-solid fa-square-plus"></i>
+              </div>
             </div>
           </div>
         ))}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,22 @@
 import { useEffect, useState } from "react";
+import ReactPaginate from "react-paginate";
 import { Api } from "../../data/Api";
 import { Card } from "../../components/GameCard/Card";
 import { Nav } from "../../components/Nav/Nav";
+import './HomeStyle.scss'
 
 export const Home = () => {
   const [data, setData] = useState([]);
+  const [currentPage, setCurrenPage] = useState(0);
+  const itemsPerPage = 30;
+
+  const pageCount = Math.ceil(data.length / itemsPerPage);
+  console.log("total de items", pageCount);
+
+  const handlePageChange = ({ selected }) => {
+    setCurrenPage(selected);
+  };
+
   useEffect(() => {
     const dataFromAPI = async () => {
       const responseData = await Api();
@@ -16,10 +28,17 @@ export const Home = () => {
 
   return (
     <>
-      {/* {data.map((game) =>(
-        <p key={game.id}>{game.title}</p>
-      ))} */}
-      <Card data={data}/>
+      <Card data={data.slice(currentPage * itemsPerPage, (currentPage +1 ) * itemsPerPage)} />
+      <ReactPaginate
+        previousLabel={"Previous"}
+        nextLabel={"Next"}
+        pageCount={pageCount}
+        marginPagesDisplayed={0}
+        pageRangeDisplayed={0}
+        onPageChange={handlePageChange}
+        containerClassName={"pagination"}
+        activeClassName={"active"}
+      />
       <Nav />
     </>
   );
