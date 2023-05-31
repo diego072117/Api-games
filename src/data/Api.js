fde76bd9a62358b1a0c0641c40e6d921f8607e29
Diego_Parra <103591603+diego072117@users.noreply.github.com>
2023-05-30 22:12:49 -0500

    feat(details-game): working details game.

--- src/data/Api.js
+++ src/data/Api.js
@@ -1,18 +1,36 @@
 import axios from "axios";
 const { VITE_RAPID_API_KEY } = import.meta.env;
 
-export const Api = async () => {
+const headers = {
+  "X-RapidAPI-Key": VITE_RAPID_API_KEY,
+  "X-RapidAPI-Host": "free-to-play-games-database.p.rapidapi.com",
+};
+
+export const getAllGames = async () => {
   try {
     const { data } = await axios.get(
       "https://free-to-play-games-database.p.rapidapi.com/api/games",
+      { headers }
+    );
+    return data;
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+};
+
+export const getGameById = async (id) => {
+  try {
+    const { data } = await axios.get(
+      "https://free-to-play-games-database.p.rapidapi.com/api/game",
       {
-        headers: {
-          "X-RapidAPI-Key": VITE_RAPID_API_KEY,
-          "X-RapidAPI-Host": "free-to-play-games-database.p.rapidapi.com",
+        headers,
+        params: {
+          id: id,
         },
       }
     );
-    return data 
+    return data;
   } catch (error) {
     console.log(error);
     return [];
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,11 +1,13 @@
 import { Routes , Route } from "react-router-dom"
 import { Home } from "../pages/Home/Home"
+import { Details } from "../pages/DetailsGame/Details"
 
 export const AppRouter = () => {
     return (
         <>
         <Routes>
             <Route path='/' element={<Home/>}/>
+            <Route path='/gameDetails/:id'  element={<Details />}/>
         </Routes>
         </>
     )
