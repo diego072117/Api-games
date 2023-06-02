387d93f6b36cfd33894480d33a993519458df80f
Diego_Parra <103591603+diego072117@users.noreply.github.com>
2023-06-02 12:35:42 -0500

    feat(detailsData): displaying detail information and refactoring of styles.

--- src/components/DetailsGame/DetailsGame.jsx
+++ src/components/DetailsGame/DetailsGame.jsx
@@ -2,12 +2,19 @@ import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./DetailsGame.scss";
 
+const MAX_CHARACTERS_TITLE = 11;
+
 export const DetailsGame = ({ detailsData }) => {
   const [openDetails, setOpenDetails] = useState(false);
 
   const contentClassName = openDetails
     ? "content-details open"
-    : "content-details close";
+    : "content-details";
+
+  const titleCharacters =
+    detailsData.title.length > MAX_CHARACTERS_TITLE
+      ? detailsData.title.slice(0, MAX_CHARACTERS_TITLE) + "..."
+      : detailsData.title;
 
   const handleClick = () => {
     setOpenDetails(!openDetails);
@@ -20,9 +27,31 @@ export const DetailsGame = ({ detailsData }) => {
           <div className="button" onClick={handleClick}>
             Overview
           </div>
-          {/* <div className="content-one">
-            <p>digital illustration</p>
-          </div> */}
+          <div className="container-content">
+            <div className="title-game">
+              <p>
+                <i className="fa-solid fa-gamepad"></i> {titleCharacters}
+              </p>
+            </div>
+            <div className="content-two">
+              <p>
+                <span className="title-details">Genre:</span>{" "}
+                {detailsData.genre}
+              </p>
+              <p>
+                <span className="title-details">Plataform:</span>{" "}
+                {detailsData.platform}
+              </p>
+              <p>
+                <span className="title-details">Publisher:</span>{" "}
+                {detailsData.publisher}
+              </p>
+              <p>
+                <span className="title-details">Developer:</span>{" "}
+                {detailsData.developer}
+              </p>
+            </div>
+          </div>
         </nav>
       </div>
     </>
